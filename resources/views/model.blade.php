5dff95a6181c2612561ccdb508febd75943e97a5
Barış Atay <114981821+barisatay0@users.noreply.github.com>
2024-04-20 21:45:58 +0300

    Model Page Design

--- resources/views/model.blade.php
+++ resources/views/model.blade.php
@@ -20,11 +20,103 @@
     <nav class="navbar d-flex justify-content-center p-2 bg-light border-bottom">
         <a href="{{ url('/') }}"><img src="{{ asset('images/Logo.png') }}" alt="Logo"></a>
     </nav>
-    <div class="w-50 text-center container">
-        
-    </div>
-</body>
+    <div class="w-50 text-center container mt-1">
+        <table class="table table-white table-hover text-center">
+            <tr>
 
+                <th scope="col"><label>Name</label><input class="form-control border-black text-center"
+                        type="text" value="modelname"></th>
+                <th scope="col"><label>İnstagram</label><input class="form-control border-black text-center"
+                        type="text" value="instagram"></th>
+            </tr>
+            <tbody>
+                <tr>
+                    <th scope="col"><label>Height</label><input class="form-control border-black text-center"
+                            type="text" value="height"></th>
+                    <th scope="col"><label>Chest Or Bust</label><input class="form-control border-black text-center"
+                            type="text" value="chest_bust"></th>
+                </tr>
+                <tr>
+                    <th scope="col"><label>Waist</label><input class="form-control border-black text-center"
+                            type="text" value="waist"></th>
+                    <th scope="col"><label>Hips</label><input class="form-control border-black text-center"
+                            type="text" value="hips"></th>
+                </tr>
+                <tr>
+                    <th scope="col"><label>Shoes</label><input class="form-control border-black text-center"
+                            type="text" value="shoes"></th>
+                    <th scope="col"><label>Eyes</label><input class="form-control border-black text-center"
+                            type="text" value="eyes"></th>
+                </tr>
+                <tr>
+                    <th scope="col">
+                        <label>Nation</label>
+                        <input class="form-control border-black text-center" type="text" value="nation">
+                    </th>
+                    <th scope="col">
+                        <label>Gender</label>
+                        <br>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input  bg-black border border-black" type="radio" name="gender"
+                                id="women">
+                            <label class="form-check-label" for="gender">Men</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input  bg-black border border-black" type="radio" name="gender"
+                                id="men" checked>
+                            <label class="form-check-label" for="flexRadioDefault2">Women</label>
+                        </div>
+                    </th>
+
+                </tr>
+                <tr>
+                    <th scope="col">
+                        <label>Busy</label>
+                        <br>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input  bg-black border border-black" type="radio" name="busy"
+                                id="0">
+                            <label class="form-check-label" for="busy">Busy</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input bg-black border border-black" type="radio" name="busy"
+                                id="1" checked>
+                            <label class="form-check-label" for="busy">Free</label>
+                        </div>
+                    </th>
+                    <th scope="col">
+                        <label>Active</label>
+                        <br>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input  bg-black border border-black" type="radio" name="active"
+                                id="1">
+                            <label class="form-check-label" for="active">Active</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input  bg-black border border-black" type="radio"
+                                name="active" id="0" checked>
+                            <label class="form-check-label" for="flexRadioDefault2">Un Active</label>
+                        </div>
+                    </th>
+                </tr>
+                <tr>
+                    <th scope="col"><label>Start</label><input class="form-control border-black text-center"
+                            type="date" data-toggle="tooltip" data-placement="top"
+                            title="Model Availability Starting"></th>
+                    <th scope="col"><label>End</label><input class="form-control border-black text-center"
+                            type="date" data-toggle="tooltip" data-placement="top"
+                            title="Model Availability Ending"></th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div></div>
+</body>
+<script>
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
 </script>
