34daf15c12f9b4a332e716c1b33b5f9a98f9f4be
Barış Atay <114981821+barisatay0@users.noreply.github.com>
2024-04-20 22:04:52 +0300

    Ajax fix

--- public/js/manager.js
+++ public/js/manager.js
@@ -18,38 +18,4 @@ window.addEventListener("resize", function () {
 if (window.innerWidth < 576) {
     sidebarContent.classList.add("hidden");
 }
-//Sidebar Copy Button 
-document.getElementById("copyButton").addEventListener("click", function () {
-    var input = document.getElementById("linker");
-    var valueToCopy = input.value;
-    var tempTextarea = document.createElement("textarea");
-    tempTextarea.style.position = "absolute";
-    tempTextarea.style.left = "-9999px";
-    tempTextarea.value = valueToCopy;
-    document.body.appendChild(tempTextarea);
-    tempTextarea.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempTextarea);
-});
-//Save Selection Button
-$('#saveSelectionBtn').click(function () {
-    var selectedModels = [];
-    $('.myCheckbox:checked').each(function () {
-        selectedModels.push($(this).closest('.card').data('id'));
-    });
 
-    $.ajax({
-        type: 'POST',
-        url: '/saveSelection',
-        data: {
-            selectedModels: selectedModels,
-            _token: '{{ csrf_token() }}'
-        },
-        success: function (response) {
-            $('#linker').val(response.encryptedData);
-        },
-        error: function (xhr, status, error) {
-            console.error(error);
-        }
-    });
-});
